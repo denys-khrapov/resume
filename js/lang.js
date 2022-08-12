dd56dbad2e653544a5c32beb2ce6e3436b0795fc
Khrapov Denys <khrapov.denys@gmail.com>
2022-08-12 03:02:12 +0300

    modified

--- js/lang.js
+++ js/lang.js
@@ -3,116 +3,164 @@
 
 const langObj = {
 	"title-page": {
-		"ua": "Резюме",
-		"en": "Resume",
+		ua: "Резюме",
+		en: "Resume",
 	},
 	"about__title": {
-		"ua": "Храпов Денис",
-		"en": "Khrapov Denys",
+		ua: "Храпов Денис",
+		en: "Khrapov Denys",
 	},
 	"about__text": {
-		"ua": "привет",
-		"en": "hello",
+		ua: "привет",
+		en: "hello",
 	},
 	"experience__title": {
-		"ua": "Досвiд роботи",
-		"en": "Work experience",
+		ua: "Досвiд роботи",
+		en: "Work experience",
 	},
 	"experience__position1": {
-		"ua": "Копірайтер",
-		"en": "Copywriter",
+		ua: "Копірайтер",
+		en: "Copywriter",
 	},
 	"experience__date1": {
-		"ua": "Вересень 2019 - Листопад 2020",
-		"en": "September 2019 - November 2020",
+		ua: "Вересень 2019 - Листопад 2020",
+		en: "September 2019 - November 2020",
 	},
 	"experience__position2": {
-		"ua": "Контент-менеджер",
-		"en": "Content Manager",
+		ua: "Контент-менеджер",
+		en: "Content Manager",
+	},
+	"experience__position3": {
+		ua: "Контент-менеджер",
+		en: "Content Manager",
 	},
 	"freelance": {
-		"ua": "Фріланс",
-		"en": "Freelance",
+		ua: "Фріланс",
+		en: "Freelance",
 	},
 	"autozvuk": {
-		"ua": "Автозвук",
-		"en": "Avtozvuk",
+		ua: "Автозвук",
+		en: "Avtozvuk",
 	},
 	"experience__date2": {
-		"ua": "Липень 2021 - Лютий 2022",
-		"en": "July 2019 - February 2020",
+		ua: "Липень 2021 - Лютий 2022",
+		en: "July 2019 - February 2020",
+	},
+	"experience__date3": {
+		ua: "Червень 2022 - теперішній час",
+		en: "June 2022 - present time",
 	},
 	"list-item1": {
-		"ua": "Робота в адмінці.",
-		"en": "Working in the admin panel.",
+		ua: "Робота в адмінці.",
+		en: "Working in the admin panel.",
 	},
 	"list-item2": {
-		"ua": "Написання технічних текстів.",
-		"en": "Writing technical texts.",
+		ua: "Написання технічних текстів.",
+		en: "Writing technical texts.",
 	},
 	"list-item3": {
-		"ua": "Обробка зображень.",
-		"en": "Image processing.",
+		ua: "Обробка зображень.",
+		en: "Image processing.",
 	},
 	"list-item4": {
-		"ua": "Створення характеристик та значень.",
-		"en": "Creating characteristics and values.",
+		ua: "Створення характеристик та значень.",
+		en: "Creating characteristics and values.",
+	},
+	"list-item01": {
+		ua: "Робота в адмінці.",
+		en: "Working in the admin panel.",
+	},
+	"list-item02": {
+		ua: "Унікальний опис категорій, товару.",
+		en: "Unique description of categories, products.",
+	},
+	"list-item03": {
+		ua: "Обробка зображень.",
+		en: "Image processing.",
+	},
+	"list-item04": {
+		ua: "Створення характеристик та значень.",
+		en: "Creating characteristics and values.",
 	},
 	"projects__title": {
-		"ua": "Проекти",
-		"en": "Projects",
+		ua: "Проекти",
+		en: "Projects",
 	},
 	"education__title": {
-		"ua": "Освіта",
-		"en": "Education",
+		ua: "Освіта",
+		en: "Education",
 	},
 	"education__name": {
-		"ua": "Донбаська державна машинобудівельна академія.",
-		"en": "Donbas state engineering academy",
+		ua: "Донбаська державна машинобудівельна академія.",
+		en: "Donbas state engineering academy",
 	},
 	"education__faculty": {
-		"ua": "Факультет: автоматизації машинобудування та інформаційних технологій.",
-		"en": "Faculty: automation of mechanical engineering and information technologies.",
+		ua: "Факультет: автоматизації машинобудування та інформаційних технологій.",
+		en: "Faculty: automation of mechanical engineering and information technologies.",
 	},
 	"education__speciality": {
-		"ua": "Спеціальність: комп'ютерні науки.",
-		"en": "Subject Area: Computer Sciences",
+		ua: "Спеціальність: комп'ютерні науки.",
+		en: "Subject Area: Computer Sciences",
 	},
 	"education__date": {
-		"ua": "Вересень 2018 - Січень 2020",
-		"en": "September 2018 - January 2020",
+		ua: "Вересень 2018 - Січень 2020",
+		en: "September 2018 - January 2020",
 	},
 	"contacts__title": {
-		"ua": "Контакти",
-		"en": "Contacts",
+		ua: "Контакти",
+		en: "Contacts",
 	},
 	"tech-skills__title": {
-		"ua": "Технічні навички",
-		"en": "Tech Skills",
+		ua: "Технічні навички",
+		en: "Tech Skills",
 	},
 	"сourses-skills__title": {
-		"ua": "Курси",
-		"en": "Courses",
+		ua: "Курси",
+		en: "Courses",
 	},
 	"language-skills__title": {
-		"ua": "Мова",
-		"en": "Language",
+		ua: "Мова",
+		en: "Language",
 	},
 	"language1": {
-		"ua": "Український - рідний",
-		"en": "Ukrainian - native",
+		ua: "Український - рідний",
+		en: "Ukrainian - native",
 	},
 	"language2": {
-		"ua": "Російська - вільно",
-		"en": "Russian - free",
+		ua: "Російська - вільно",
+		en: "Russian - free",
 	},
 	"language3": {
-		"ua": "Англійська - базовий",
-		"en": "English - base ",
+		ua: "Англійська - базовий",
+		en: "English - base ",
 	},
 	"team-work": {
-		"ua": "команда робота",
-		"en": "team work",
+		ua: "команда робота",
+		en: "team work",
+	},
+	"base1": {
+		ua: "Базовий рівень",
+		en: "Basic level",
+	},
+	"base2": {
+		ua: "Базовий рівень",
+		en: "Basic level",
+	},
+	"lvl1": {
+		ua: "Html Academy Html и Css Рівень 1",
+		en: "Html Academy Html и Css Level 1",
+	},
+	"lvl2": {
+		ua: "Html Academy Html и Css Рівень 2",
+		en: "Html Academy Html и Css Level 2",
+	},
+	"lvljs1": {
+		ua: "Udemy Основи JavaScript",
+		en: "Udemy JavaScript Basics",
+	},
+	"lvljs2": {
+		ua: "Udemy Просунутий JavaScript",
+		en: "Udemy Advanced JavaScript",
 	},
 };
 
